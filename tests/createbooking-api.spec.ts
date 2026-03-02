f67ca040a40e1fb9470e6646c2147f04092f58b5
pisit-wlb <pisit@welovebug.com>
2026-03-02 10:44:08 +0700

    [Added] เพิ่ม Test Step ในการ Create Booking เพื่อสร้างการจอtests/createbooking-api.spec.ts

--- tests/createbooking-api.spec.ts
+++ tests/createbooking-api.spec.ts
@@ -18,4 +18,41 @@ test("เข้าสู่ระบบและ การจองด้วย 
 		});
 		token = responseBody.token;
 	});
+
+	await test.step("สร้างการจองที่พัก firstname: Pisit และ lastname: Wanakitrungrueng และตรวจสอบผลการจองที่พัก", async () => {
+		const responseCreateBooking = await request.post("https://restful-booker.herokuapp.com/booking", {
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			data: {
+				"firstname": "Pisit",
+				"lastname": "Wanakitrungrueng",
+				"totalprice": 111,
+				"depositpaid": true,
+				"bookingdates": {
+					"checkin": "2026-03-07",
+					"checkout": "2026-03-10"
+				},
+				"additionalneeds": "Spare-bed and Dinner"
+			}
+		});
+		expect(responseCreateBooking.ok()).toBeTruthy();
+		const responseBody = await responseCreateBooking.json();
+		await test.info().attach("API-Booking Response", {
+			body: JSON.stringify(responseBody, null, 2),
+			contentType: "application/json"
+		});
+				expect(responseBody.booking).toMatchObject({
+			"firstname": "Pisit",
+			"lastname": "Wanakitrungrueng",
+			"totalprice": 111,
+			"depositpaid": true,
+			"bookingdates": {
+				"checkin": "2026-03-07",
+				"checkout": "2026-03-10"
+			},
+			"additionalneeds": "Spare-bed and Dinner"
+		})
+	});
 });
